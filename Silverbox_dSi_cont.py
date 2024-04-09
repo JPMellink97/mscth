5413b3b78fa8f1ea12a7da690df33713a40d3747
JPMellink97 <j.p.mellink@student.tue.nl>
2024-04-09 08:54:02 +0000

    CT SUBNET run

--- Silverbox_dSi_cont.py
+++ Silverbox_dSi_cont.py
@@ -37,18 +37,18 @@ test.dt = dt
 
 tau = train.dt/0.03
 
-fit_sys_ss_enc = deepSI.fit_systems.SS_encoder_deriv_general(nx=2, na=10, nb=10, tau=tau)
+fit_sys_ss_enc = deepSI.fit_systems.SS_encoder_deriv_general(nx=2, na=20, nb=20, tau=tau)
 train_enc, val_enc = train.train_test_split(split_fraction=0.2)
 
 #Start fitting
 fit_sys_ss_enc.fit(train_sys_data=train_enc, val_sys_data=val_enc, \
-                   epochs=100, batch_size=256, loss_kwargs={'nf':100}) #nf is T in paper
+                   epochs=500, batch_size=256, loss_kwargs={'nf':80}) #nf is T in paper
 
 print(fit_sys_ss_enc.fn) #state network x_t+1 = f([x,u])
 print(fit_sys_ss_enc.hn) #output network y_t = h(x_t)
 print(fit_sys_ss_enc.encoder) #encoder network x_t = psi([upast, ypast])
 
-# test_sim_enc = fit_sys_ss_enc.apply_experiment(test, save_state=True)
+test_sim_enc = fit_sys_ss_enc.apply_experiment(test, save_state=True)
 # np.savetxt("ct_subnet_states.csv", test_sim_enc.x, delimiter=",")
 
 # fit_sys_IO.save_system('IO-sys')
@@ -59,7 +59,7 @@ plt.plot(test.y)
 plt.plot(test.y - test_sim_enc.y)
 plt.title(f'test set simulation SS encoder, NRMS = {test_sim_enc.NRMS(test):.2%}')
 plt.show()
-plt.savefig('test_res_sim.png')
+plt.savefig('test_res_sim_v2.png')
 plt.close()
 
 # train_sim_enc = fit_sys_ss_enc.apply_experiment(train)
